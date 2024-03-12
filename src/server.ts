5c68bca17372dcf534a50992406e74563fa3e173
MaiconGlobes <maico-globes@hotmail.com>
2024-03-11 23:37:39 -0300

    Ajustes e configuração de CORS

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import bodyParser from "body-parser";
 import routes from "./configs/route.config";
 import ValidarEntradasSistemaMiddleware from './middlewares/validar_entradas.middleware';
+import { NextFunction, Request, Response } from "express";
 
 const cors = require('cors');
 const express = require('express')
 const app = express()
 
-//app.use(cors());
-app.use(cors({
-  origin: 'http://localhost:3005'
-}));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 
